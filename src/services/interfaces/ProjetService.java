86941a57049baeed2d514c91bc3dbe39cd02e589
MahaDaiki <142234887+MahaDaiki@users.noreply.github.com>
2024-09-20 11:00:59 +0100

    BAT-22 Projet Service

---
+++ src/services/interfaces/ProjetService.java
@@ -0,0 +1,12 @@
+package services.interfaces;
+
+import entities.Projet;
+
+import java.util.List;
+
+public interface ProjetService {
+    void addProjet(Projet projet, int client_id);
+    List<Projet> getAllProjets();
+    void updateProjet(Projet projet, int projet_id);
+    void updateProjetStatus(Projet projet, int projet_id);
+}
