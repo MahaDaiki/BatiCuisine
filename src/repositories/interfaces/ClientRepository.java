b1c773afc68a6fa9c9a7afd562fcc4acfd5ec139
MahaDaiki <142234887+MahaDaiki@users.noreply.github.com>
2024-09-22 17:01:39 +0100

    BAT-27 display de projet

--- src/repositories/interfaces/ClientRepository.java
+++ src/repositories/interfaces/ClientRepository.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 public interface ClientRepository {
    void add(Client client);
    Optional<Client> findClientByName(String name);
-   List<Client> getAllClientsWithProjets();
+   List<Client> getAllClientsWithProjets(int clientId);
 
 }
